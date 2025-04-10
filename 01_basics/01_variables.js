acedfc00592c2bc5af30d4912ae4c99ba3648ee2
Akashkumar888 <akashgkr12@gmail.com>
2025-04-11 01:39:46 +0530

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=143124
+var accountEmail="akash@gmail.com"
+let c=accountPassword="123456"
+accountCity="Bangalore"
+
+
+// accountId="54321"
+
+accountEmail="priya9893@gmail.com"
+
+accountPassword="9876543"
+accountCity="Mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
---
+++ Harsh_Bhaiya/index.js
@@ -0,0 +1,8 @@
+
+
+var a=[12,13];
+var b=a;
+a.pop();
+console.log(a);
+console.log(b);
+
