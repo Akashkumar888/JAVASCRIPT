3d1ead35253254133eced5167a59a2d7c6592cb3
Akashkumar888 <akashgkr12@gmail.com>
2025-05-19 19:35:00 +0530

    update

---
+++ Harsh_Bhaiya/obj.js
@@ -0,0 +1,16 @@
+
+var ghadi={
+brand:'sonnata',
+price:'16M',
+rating : 5,
+type:'wrist watch',
+digital:false,
+kuchhbolo:function(){
+  
+}
+};
+
+
+console.log(ghadi.rating);
+console.log(ghadi.price);
+
